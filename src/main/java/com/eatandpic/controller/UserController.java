7ebf7de8805ecf0cbd6fd18c497ec361f497776d
gonvazsua <gonzalovazquezs@gmail.com>
2017-02-25 19:32:07 +0100

    UserController

--- src/main/java/com/eatandpic/controller/UserController.java
+++ src/main/java/com/eatandpic/controller/UserController.java
@@ -1,19 +1,14 @@
 package com.eatandpic.controller;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -79,6 +74,8 @@ public class UserController {
 				  existingUser = userDao.findByUsername(user.getUsername());
 				  
 				  if(existingUser != null && existingUser.checkPassword(user.getPassword())){
+					  existingUser.setLastLogin(new Date());
+					  userDao.save(existingUser);
 					  response.setStatus(HttpServletResponse.SC_ACCEPTED);
 				  }
 				  else{
